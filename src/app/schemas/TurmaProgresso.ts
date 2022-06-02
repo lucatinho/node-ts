c0f1841d4573c26190886f9ea2368089918f05a2
edjunior <lucatinho_1@hotmail.com>
2022-06-02 13:48:05 -0300

    reformulando progresso

--- src/app/schemas/TurmaProgresso.ts
+++ src/app/schemas/TurmaProgresso.ts
@@ -2,34 +2,44 @@ import {model, Document} from 'mongoose';
 import mongoose from "mongoose";
 
 interface TurmaProgressoInterface extends Document {
-    nome: string;
-    cursos: Array<any>;
-    qtdAulas?: Number;
-    isActive: boolean;
-    createdAt?: Date;
-    finish: Date;
-    users?: Array<any>;
+    user: string;
+    turma: string;
+    progress?: Array<Progress>;
+}
+
+interface Progress {
+    curso: string,
+    qtdAulaView: number,
+    atrasado: boolean
 }
 
 const TurmaProgressoSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     turma: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'TurmaProgresso',
         required: true
     },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    qtdAulaView: {
-        type: Number,
-        default: 0
-    },
-    atrasado: {
-        type: Boolean,
-        require: true,
-        default: false
-    }
+    progress: [
+        {
+            curso: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Curso',
+                required: true
+            },
+            qtdAulaView: {
+                type: Number,
+                default: 0
+            },
+            atrasado: {
+                type: Boolean,
+                default: false
+            }
+        }
+    ]
 });
 
 
